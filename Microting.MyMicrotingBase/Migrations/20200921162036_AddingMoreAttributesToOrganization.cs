bc68902cb0c9703d42fa4a794c5f6f6793926a2a
René Schultz Madsen <rm@microting.dk>
2020-09-21 16:31:04 +0000

    Adding the missing DBMigrator.

---
+++ Microting.MyMicrotingBase/Migrations/20200921162036_AddingMoreAttributesToOrganization.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Microting.MyMicrotingBase.Migrations
+{
+    public partial class AddingMoreAttributesToOrganization : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PluginGroupPermissionVersions_PluginPermissions_PermissionId",
+                table: "PluginGroupPermissionVersions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PluginGroupPermissionVersions_PluginGroupPermissions_PluginG~",
+                table: "PluginGroupPermissionVersions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PluginGroupPermissionVersions_PermissionId",
+                table: "PluginGroupPermissionVersions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PluginGroupPermissionVersions_PluginGroupPermissionId",
+                table: "PluginGroupPermissionVersions");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerNo",
+                table: "OrganizationVersions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "PaymentOverdue",
+                table: "OrganizationVersions",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentStatus",
+                table: "OrganizationVersions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "OrganizationVersions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerNo",
+                table: "Organizations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "PaymentOverdue",
+                table: "Organizations",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentStatus",
+                table: "Organizations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Organizations",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomerNo",
+                table: "OrganizationVersions");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentOverdue",
+                table: "OrganizationVersions");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentStatus",
+                table: "OrganizationVersions");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "OrganizationVersions");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerNo",
+                table: "Organizations");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentOverdue",
+                table: "Organizations");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentStatus",
+                table: "Organizations");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Organizations");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PluginGroupPermissionVersions_PermissionId",
+                table: "PluginGroupPermissionVersions",
+                column: "PermissionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PluginGroupPermissionVersions_PluginGroupPermissionId",
+                table: "PluginGroupPermissionVersions",
+                column: "PluginGroupPermissionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PluginGroupPermissionVersions_PluginPermissions_PermissionId",
+                table: "PluginGroupPermissionVersions",
+                column: "PermissionId",
+                principalTable: "PluginPermissions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PluginGroupPermissionVersions_PluginGroupPermissions_PluginG~",
+                table: "PluginGroupPermissionVersions",
+                column: "PluginGroupPermissionId",
+                principalTable: "PluginGroupPermissions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
